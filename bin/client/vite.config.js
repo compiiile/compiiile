ab08484ce5ab1e69d6658d8cd1a6f2fb4a716507
AlbanCrepel <alban.crepel@gmail.com>
2023-10-03 09:22:04 +0200

    :wrench: fix image resolving

--- bin/client/vite.config.js
+++ bin/client/vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite'
+import {defineConfig,searchForWorkspaceRoot} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import compiiile from "./vitePluginCompiiile"
 
@@ -8,5 +8,13 @@ export default defineConfig({
         vue(),
         compiiile()
     ],
-    base: "./"
+    base: "./",
+    server: {
+        fs: {
+            allow: [
+                searchForWorkspaceRoot(process.cwd()),
+                '.'
+            ],
+        },
+    },
 })
